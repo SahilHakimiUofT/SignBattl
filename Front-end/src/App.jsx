dd54b869e409448e30cfe205b41d5355b763abbe
SahilHakimiUofT <71340008+SahilHakimiUofT@users.noreply.github.com>
2022-10-16 03:58:11 -0400

    game working-ish

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import GameComp from "./components/Game/gameComp";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/game" element={<GameComp />} />
         </Routes>
       </BrowserRouter>
     </div>

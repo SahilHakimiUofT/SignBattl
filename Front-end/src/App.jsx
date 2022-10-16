732c4598e13d5f6986ab78bd17a47a4325d248fd
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-10-16 04:07:57 -0400

    worked on alphabet

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Lesson from "./components/Home/Lessons";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/lesson" element={<Lesson />} />
         </Routes>
       </BrowserRouter>
     </div>
